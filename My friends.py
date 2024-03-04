0e3ba87f285802ad6f01dd6ef1b06028a583e623
Aisha Zayan <aisha.mzayan@gmail.com>
2024-03-04 06:46:33 +0300

    My friends

---
+++ My friends.py
@@ -0,0 +1,12 @@
+Friends = ["Mareej" , "Jana" , "Jomanah" , "Mayar" , "Manar" , "Mariam" , "Fatma" , "Habiba"]
+
+print(f"{Friends}\nThis is a list of my friends \n")
+
+print(f"My BFF called {Friends[0]}. \n ")
+print(f"There are two of my friends called {Friends[1]}. They are beautifull. \n")
+print(f"My friend {Friends[2]} is so friendly. I love her. \n")
+print(f"My friend {Friends[3]} likes playing with the ball. \n")
+print(f"My friend {Friends[4]} likes to go shopping. \n")
+print(f"My friend {Friends[-3]} is so kind. \n")
+print(f"My friend {Friends[-1]} is so funny.She is my sister's BFF . \n")
+print(f"The last friend called {Friends[-2]}. She is the youngest one of us.")
