82379674836987c9797adf19e3ba786255b658ff
Aisha Zayan <aisha.mzayan@gmail.com>
2024-03-04 07:09:41 +0300

    Basket

---
+++ Basket.py
@@ -0,0 +1,11 @@
+basket = [["Apples" , "Banans"] , ["Milk" , "Water"]]
+print(basket)
+input("Press enter to change the content .....")
+basket[0].insert(0 , "oranges")
+basket[0].append("Kiwis")
+basket[1].insert(0 , "Coffee")
+basket[1].remove("Water")
+basket[1].append("Tea")
+basket.append( [1 , 2 , 3] )
+print("Here is the updated basket")
+print(basket)
