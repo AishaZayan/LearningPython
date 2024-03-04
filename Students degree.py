18d4e606f48c05f91abcea532e65c4449462c295
Aisha Zayan <aisha.mzayan@gmail.com>
2024-03-04 06:52:02 +0300

    Students degree

---
+++ Students degree.py
@@ -0,0 +1,9 @@
+degree = int(input("Enter the degree of the exam \n"))
+if degree >= 90:
+    print("Excellence")
+elif degree >= 70:
+  print ("Good")
+elif degree >= 50:
+  print ("acceptable")
+else:
+  print ("fail")
