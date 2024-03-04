9383a01e4939ba8dd9d475472a264ad2685b1122
Aisha Zayan <aisha.mzayan@gmail.com>
2024-03-04 06:45:52 +0300

    YouTube channel name generator

---
+++ YouTube channel name generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the YouTube Channel Name Genetor :")
+nickname = input('What is your nickname? \n')
+about = input('What is your channel about? \n')
+print("you could name your channel" +  "(" + about + ' with ' + nickname +")")
