6f21d999b5e125d9ab4ae8176170f941f051279c
Aisha Zayan <aisha.mzayan@gmail.com>
2024-10-07 22:56:15 +0300

    decoding

---
+++ decoding.py
@@ -0,0 +1,29 @@
+import string
+
+
+
+def play(message, shift):
+  alphabet = string.ascii_lowercase
+
+  encrypted_message = ""
+
+  for letter in message:
+    if letter.lower() in alphabet:
+      original_position = alphabet.index(letter.lower())
+      new_position = (original_position - shift) % 26
+      encrypted_letter = alphabet[new_position]
+      if letter.isupper():
+        encrypted_letter = encrypted_letter.upper()
+
+      encrypted_message += encrypted_letter
+    else:
+      encrypted_message += letter
+
+  print(
+      f"\n\nHere is the original message\n*****\n\n{encrypted_message}\n\n*****" )
+
+
+user_massage = input("Enter a messsage: ")
+shift_number = int(input("Enter a shift number:"))
+
+play(message=user_massage, shift=shift_number)
